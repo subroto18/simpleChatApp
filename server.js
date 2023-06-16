28f285c72cc2276e81208f82209bf5297fccba4a
subroto18 <subrotochakraborty18@gmail.com>
2023-06-16 10:47:23 +0530

    pushing all

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  socket.on("form", (data) => {
+    io.emit("send_message", data.message);
+  });
+});
+
+server.listen();
